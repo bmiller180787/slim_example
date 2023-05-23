ded4c366cea75560bac0981fb309fbaf36e6693c
Brent <bmiller180787@gmail.com>
2023-05-23 13:51:38 +0100

    practicing routing types

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 use App\Controllers\CoursesAPIController;
 use Slim\App;
+use \Psr\Http\Message\RequestInterface;
+use \Psr\Http\Message\ResponseInterface;
 use Slim\Views\PhpRenderer;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
@@ -11,11 +13,32 @@ return function (App $app) {
 
     //demo code - two ways of linking urls to functionality, either via anon function or linking to a controller
 
-    $app->get('/', function ($request, $response, $args) use ($container) {
+    $app->get('/', function (RequestInterface $request, ResponseInterface $response, $args) use ($container) {
         $renderer = $container->get(PhpRenderer::class);
         return $renderer->render($response, "index.php", $args);
     });
 
+
     $app->get('/courses', CoursesAPIController::class);
 
+// Types of Routes
+
+    // Anon function - don't do it like this
+
+    $app->get('/anon', function (RequestInterface $request, ResponseInterface $response, $args): ResponseInterface
+    {
+        $response->getBody()->write('hello from an anon function');
+        return $response;
+    });
+
+    //Names function
+
+    $app->get('/named', 'namedfunc');
+
 };
+
+function namedfunc (RequestInterface $request, ResponseInterface $response, $args): ResponseInterface
+{
+    $response->getBody()->write('hello from a named function');
+    return $response;
+}
